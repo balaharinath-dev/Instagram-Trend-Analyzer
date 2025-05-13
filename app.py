ea8004e9f6243a601a86e4d975fd7dfbcada87f0
Balaharinath C <balaharinath.dev@gmail.com>
2025-05-13 21:02:05 +0530

    Commit

--- app.py
+++ app.py
@@ -323,27 +323,27 @@ Make it comprehensive, engaging, and clearly structured in en-US English.
         "url_list": url_list
     }
     
-st.sidebar.markdown("""
-        <style>
-            .red-button {
-                display: inline-block;
-                padding: 0.5em 1em;
-                color: white !important;
-                background-color: orange !important;
-                border-radius: 15px !important;
-                text-decoration: none !important;
-                font-weight: bold;
-                text-align: center;
-                width: 30%;
-                margin-bottom: 10px;
-            }
-            .red-button:hover {
-                background-color: orange !important; /* darker on hover */
-            }
-        </style>
+# st.sidebar.markdown("""
+#         <style>
+#             .red-button {
+#                 display: inline-block;
+#                 padding: 0.5em 1em;
+#                 color: white !important;
+#                 background-color: orange !important;
+#                 border-radius: 15px !important;
+#                 text-decoration: none !important;
+#                 font-weight: bold;
+#                 text-align: center;
+#                 width: 30%;
+#                 margin-bottom: 10px;
+#             }
+#             .red-button:hover {
+#                 background-color: orange !important; /* darker on hover */
+#             }
+#         </style>
 
-        <a href="http://localhost:8501" target="_self" class="red-button">Back</a>
-    """, unsafe_allow_html=True)
+#         <a href="http://localhost:8501" target="_self" class="red-button">Back</a>
+#     """, unsafe_allow_html=True)
 
 # Sidebar UI with Instagram theme
 with st.sidebar:
